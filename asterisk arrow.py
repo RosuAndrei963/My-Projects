e2a0be0e4bbb5008a7101f748aadbacb403a26f6
Rosu Andrei <139537277+RosuAndrei963@users.noreply.github.com>
2025-05-20 20:56:34 +0300

    Create asterisk arrow.py

---
+++ asterisk arrow.py
@@ -0,0 +1,10 @@
+# last edited 28 sept 2023
+
+for x in range (0,6):
+    for y in range (x):
+        print("*",end=" ")
+    print(" ")
+for a in range (6,0,-1):
+    for b in range (a):
+        print("*",end=" ")
+    print(" ")
